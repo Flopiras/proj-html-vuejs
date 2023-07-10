067126724edbbd782cece752ee1fe22090c26370
Flopiras <Flopiras@yahoo.it>
2023-07-10 16:06:57 +0200

    add logo and BaseDropDown

--- src/components/header/HeaderMain.vue
+++ src/components/header/HeaderMain.vue
@@ -1,8 +1,18 @@
+<script>
+import { navbarOptions } from '../../data';
+import BaseDropdown from '../BaseDropdown.vue';
+
+export default {
+    components: { BaseDropdown }
+}
+</script>
+
 <template>
     <div id="header-main">
         <div class="container">
             <img src="../../assets/img/logo.png" alt="logo">
 
+            <BaseDropdown v-for="option in navbarOptions" :key="option" />
         </div>
     </div>
 </template>
