025ca12cf6e5a914529ed136a58e3e9caddf0b9c
Flopiras <Flopiras@yahoo.it>
2023-07-10 14:43:41 +0200

    add address and hours

--- src/components/header/HeaderTop.vue
+++ src/components/header/HeaderTop.vue
@@ -1,5 +1,30 @@
 <template>
-    <div class="top-header"></div>
+    <div class="top-header">
+        <div class="containier d-flex justify-content-between">
+            <!-- indirizzi -->
+            <div class="address">
+                <ul>
+                    <li>
+                        <h6>North baukhlula,tala, USA</h6>
+                    </li>
+                    <li>
+                        <h6>demo@example.com</h6>
+                    </li>
+
+                </ul>
+            </div>
+            <div>
+                <div class="hours">
+                    <p>8.30AM-8.30PM</p>
+                </div>
+                <div class="social">
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
@@ -8,5 +33,20 @@
 .top-header {
     background-color: $main-color;
     min-height: 50px;
+    color: white;
+}
+
+.top-header,
+h6 {
+
+    font-size: 0.9rem;
+}
+
+ul {
+    display: flex;
+
+    li {
+        margin-right: 1rem;
+    }
 }
 </style>
