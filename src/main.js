0516b4cb39f790ad047685f9a6602a8703e1f50b
Flopiras <Flopiras@yahoo.it>
2023-07-10 14:58:24 +0200

    add icons from FontAwesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,31 @@ import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import { 
+    faFacebookF,
+    faTwitter,
+    faInstagram,
+    faPinterestP
+ } from '@fortawesome/free-brands-svg-icons';
+
+import { 
+    faLocationDot, 
+    faEnvelope, 
+    faMagnifyingGlass,
+    faCartShopping 
+} from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add(faClock, faLocationDot, faEnvelope, faMagnifyingGlass, faCartShopping, faFacebookF, faTwitter, faInstagram, faPinterestP);
+
+createApp(App)
+.component('FontAwesomeIcon', FontAwesomeIcon)
+.mount('#app')
