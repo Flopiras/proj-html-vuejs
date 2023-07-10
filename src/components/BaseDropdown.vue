005ec3104e5a7ee292c2bedcb7b7d05e8ce42b39
Flopiras <Flopiras@yahoo.it>
2023-07-10 16:34:55 +0200

    edit menu options

--- src/components/BaseDropdown.vue
+++ src/components/BaseDropdown.vue
@@ -11,9 +11,11 @@ export default {
         <button class="btn btn-link dropdown-toggle">{{ placeholder }}</button>
 
         <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">HOME-1</a></li>
+            <li><a class="dropdown-item" href="#">HOME-2</a></li>
+            <li><a class="dropdown-item" href="#">HOME-3</a></li>
+            <li><a class="dropdown-item" href="#">HOME-LANDING</a></li>
+            <li><a class="dropdown-item" href="#">HOME-VIDEO</a></li>
         </ul>
     </div>
 </template>
