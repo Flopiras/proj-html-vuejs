4ee7a03d07a97705b65f97660b7605fc154be755
Flopiras <Flopiras@yahoo.it>
2023-07-11 17:06:51 +0200

    add content OfferCard

---
+++ src/components/main/OfferCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    props: { text: String }
+}
+</script>
+
+<template>
+    <div class="offer-card">
+        <div class="container">
+            <div class="row">
+                <div class="col-8">{{ text }}</div>
+                <div class="col-4"></div>
+            </div>
+            <div class="row">
+                <div class="col-8 text-uppercase">shop now</div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.offer-card {
+    font-weight: 600;
+    color: white;
+}
+</style>
