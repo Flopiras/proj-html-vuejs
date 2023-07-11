8caa56d917d7c9040ff407d48ac2fd54256dc63e
Flopiras <Flopiras@yahoo.it>
2023-07-11 11:43:12 +0200

    add products in index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,21 @@
 const navbarOptions = ['Home', 'About', 'Service', 'Portfolio', 'Shop', 'Blog', 'Contact'];
 
-export{navbarOptions};
+const products = [
+    {
+        "id": 1,
+        "name": "vegetable",
+        "pic": "fe1.jpeg"
+    },
+    {
+        "id": 2,
+        "name": "fruit",
+        "pic": "fe2.jpeg"
+    },
+    {
+        "id": 3,
+        "name": "milk",
+        "pic": "fe3.jpeg"
+    }
+]
+
+export{navbarOptions, products};
