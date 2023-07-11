dd9469aec08b19fe28bd09e46da766c7dd5c780b
Flopiras <Flopiras@yahoo.it>
2023-07-11 10:37:53 +0200

    add section paragraph

--- src/components/main/SectionMission.vue
+++ src/components/main/SectionMission.vue
@@ -1,19 +1,45 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-2"></div>
-            <div class="col-4">
-                <h2 class="main-color fw-bold">
-                    <span class="second-color">Look what</span> consumer power has done with <span
-                        class="second-color">organic food;</span> we can do the same with clothes.
-                </h2>
+    <section id="mission">
+        <div class="container">
+            <div class="row">
+                <div class="col-2"></div>
+                <!-- we can -->
+                <div class="col-4 ">
+                    <h2 class="main-color fw-bold">
+                        <span class="second-color">Look what</span> consumer power has done with <span
+                            class="second-color">organic food;</span> we can do the same with clothes.
+                    </h2>
+                </div>
+                <div class="col-4">
+                    <p>
+                        I have a need to make sorts of connections literal sometimes, and a vehicle often to do that.
+                        I have a relationship to hosting culture. <span class="second-color">Ii isn't really about it helps
+                            to do that. I have a relatioonship to hosting culture.</span>
+                    </p>
+                    <p>
+                        Need to make these sort of connections literal sometimes, and a vehicle often to do that.
+                        I have a relationship to hosting culture.
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
 @use '../../assets/sass/vars' as *;
+
+.col-4 {
+
+    h2 {
+        font-size: 2.5rem;
+    }
+
+    p {
+        padding-top: 1rem;
+        font-size: 0.8rem;
+    }
+}
 
 .main-color {
     color: $main-color;
