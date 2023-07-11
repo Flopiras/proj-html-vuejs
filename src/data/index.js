378b9aea82263869a64f84b370080578d4dbc865
Flopiras <Flopiras@yahoo.it>
2023-07-11 15:32:24 +0200

    add discount foods

--- src/data/index.js
+++ src/data/index.js
@@ -23,6 +23,7 @@ const foods = [
         'id': 1,
         'name': 'Fress Apple',
         'pic': 's5-150x150.jpg',
+        'discount': false,
         'price': null,
         'lastPrice': '$18'
     },
@@ -30,6 +31,7 @@ const foods = [
         'id': 2,
         'name': 'Fress Cobie',
         'pic': 's2-150x150.jpg',
+        'discount': true,
         'price': '$18',
         'lastPrice': '$17'
     },
@@ -37,6 +39,7 @@ const foods = [
         'id': 3,
         'name': 'Fresh Watermelon',
         'pic': 's8-150x150.jpg',
+        'discount': false,
         'price': null,
         'lastPrice': '$15'
     },
@@ -44,6 +47,7 @@ const foods = [
         'id': 4,
         'name': 'Organic Juice',
         'pic': 's1-150x150.jpg',
+        'discount': true,
         'price': '$20',
         'lastPrice': '$15'
     },
@@ -51,6 +55,7 @@ const foods = [
         'id': 5,
         'name': 'Fress Blueberries',
         'pic': 's4-150x150.jpg',
+        'discount': true,
         'price': '$19',
         'lastPrice': '$14'
     },
@@ -59,12 +64,14 @@ const foods = [
         'name': 'Organic Letus',
         'pic': 's10-150x150.jpg',
         'price': null,
+        'discount': false,
         'lastPrice': '$23'
     },
     {
         'id': 7,
         'name': 'Red Gajor',
         'pic': 's9-150x150.jpg',
+        'discount': true,
         'price': '$29',
         'lastPrice': '$26'
     },
@@ -73,6 +80,7 @@ const foods = [
         'name': 'Naga Pepper',
         'pic': 's7-150x150.jpg',
         'price': null,
+        'discount': false,
         'lastPrice': '$21'
     }
 ]
