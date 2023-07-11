530a15a5373aa59f555921277f0f0b4acf62e9b1
Flopiras <Flopiras@yahoo.it>
2023-07-11 18:26:11 +0200

    add section-header

--- src/components/main/SectionTrending.vue
+++ src/components/main/SectionTrending.vue
@@ -1,5 +1,34 @@
 <template>
-    <div class="container">
-        <div class="section-header"></div>
-    </div>
+    <section>
+
+        <div class="container">
+            <div class="section-header">
+                <h5 class="second-color">Running week top selling</h5>
+                <h3 class="main-color text-uppercase">top <span class="second-color">trending</span> organic food</h3>
+            </div>
+        </div>
+    </section>
 </template>
+
+<style lang="scss" scoped>
+section {
+    margin: 3rem 0;
+}
+
+.section-header {
+    text-align: center;
+    padding-top: 3rem;
+    background-image: url(../../assets/img/title-shap.png);
+    background-repeat: no-repeat;
+    background-position: 50%;
+
+    h5 {
+        font-weight: 600;
+    }
+
+    h3 {
+        font-size: 2.3rem;
+        font-weight: 600;
+    }
+}
+</style>
