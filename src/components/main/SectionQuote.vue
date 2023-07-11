d48876a04c1375a8218fbcf6745bf97620d6a8fb
Flopiras <Flopiras@yahoo.it>
2023-07-11 16:40:21 +0200

    add quote

--- src/components/main/SectionQuote.vue
+++ src/components/main/SectionQuote.vue
@@ -4,6 +4,13 @@
             <div class="marks">
                 <FontAwesomeIcon icon="fa-quote-left" class="fa-2xl" />
             </div>
+            <div class="quote">
+                <p>
+                    "Dessert pudding dessert jelly bean cupcake sweet caramels gingerbread. Fuitcake biscuit cheesecake.
+                    Cookie topping sweets muffine pudding tart bear claw sugar plum croissant.". We started as a small legal
+                    consultancy. We have proved our competence and had mani satisfied clients.
+                </p>
+            </div>
         </div>
     </section>
 </template>
@@ -18,5 +25,13 @@ section {
 .marks {
     font-size: 3rem;
     color: $main-color;
+}
+
+.quote {
+    width: 60%;
+    margin: 1rem auto;
+    font-weight: 600;
+    font-size: 1.2rem;
+
 }
 </style>
