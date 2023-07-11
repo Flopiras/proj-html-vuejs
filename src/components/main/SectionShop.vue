1d4797875449dc669529fdc096dadf519d11bd74
Flopiras <Flopiras@yahoo.it>
2023-07-11 12:30:31 +0200

    add background

---
+++ src/components/main/SectionShop.vue
@@ -0,0 +1,14 @@
+<template>
+    <section id="shop">
+        <div class="container">
+
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    background-image: url(../../assets/img/shop-bg-img.jpg);
+    min-height: 100px;
+}
+</style>
