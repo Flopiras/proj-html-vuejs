b2ab0f5a06546a04756a1e4d93afe20c1b174522
Flopiras <Flopiras@yahoo.it>
2023-07-11 17:02:55 +0200

    add cols

---
+++ src/components/main/SectionOffers.vue
@@ -0,0 +1,21 @@
+<script>
+import OfferCard from './OfferCard.vue';
+
+export default {
+    components: { OfferCard }
+}
+</script>
+
+<template>
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col-4"></div>
+                <div class="col-4"></div>
+                <div class="col-2"></div>
+            </div>
+        </div>
+        <OfferCard />
+    </div>
+</template>
