d1ac4bf7857ae421584675cbabaf81b28a892bbc
Flopiras <Flopiras@yahoo.it>
2023-07-11 10:20:01 +0200

    add 2 upper cols

---
+++ src/components/main/SectionMission.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-4">
+                <h2 class="main-color fw-bold">
+                    <span class="second-color">Look what</span> consumer power has done with <span
+                        class="second-color">organic food;</span> we can do the same with clothes.
+                </h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/sass/vars' as *;
+
+.main-color {
+    color: $main-color;
+}
+
+.second-color {
+    color: $second-main-color;
+}
+</style>
