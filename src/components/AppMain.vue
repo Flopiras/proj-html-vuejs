454c57d5768addf21946760cf759be22260a9857
Flopiras <Flopiras@yahoo.it>
2023-07-11 15:53:46 +0200

    add SectionQuote

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,10 @@
 <script>
 import SectionMission from './main/SectionMission.vue';
 import SectionShop from './main/SectionShop.vue';
+import SectionQuote from './main/SectionQuote.vue';
 
 export default {
-    components: { SectionMission, SectionShop },
+    components: { SectionMission, SectionShop, SectionQuote },
     props: { sectionProducts: Array, shopProducts: Array }
 }
 </script>
@@ -14,5 +15,7 @@ export default {
         <SectionMission :products="sectionProducts" />
         <!-- shop -->
         <SectionShop :foods="shopProducts" />
+        <!-- quote -->
+        <SectionQuote />
     </main>
 </template>
