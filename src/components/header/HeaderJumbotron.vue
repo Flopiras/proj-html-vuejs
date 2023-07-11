7b1d672edff24a5cc2781dc8a9b954c51df997dc
Flopiras <Flopiras@yahoo.it>
2023-07-11 09:57:44 +0200

    add jumbotron content

--- src/components/header/HeaderJumbotron.vue
+++ src/components/header/HeaderJumbotron.vue
@@ -1,12 +1,31 @@
 <template>
     <div id="jumbotron">
+        <div class="container">
 
+            <div class="jumbo-content text-center">
+                <h5>GOGRIN ORGANIC FOOD STORE</h5>
+                <H1>Eating naturally & orgainc is always be healty.</H1>
+            </div>
+        </div>
     </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
 #jumbotron {
     background-image: url(../../assets/img/slider.jpg);
-    height: 900px;
+    height: 850px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.jumbo-content {
+    color: white;
+
+    h1 {
+        font-weight: 700;
+        font-size: 6rem;
+
+    }
 }
 </style>
