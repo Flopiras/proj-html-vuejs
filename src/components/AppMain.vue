137f23b69a57f0653367f2184989b01ccbb8f409
Flopiras <Flopiras@yahoo.it>
2023-07-11 16:50:55 +0200

    add SectionOffers

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,8 +3,9 @@ import SectionMission from './main/SectionMission.vue';
 import SectionShop from './main/SectionShop.vue';
 import SectionQuote from './main/SectionQuote.vue';
 
+import SectionOffers from './main/SectionOffers.vue';
 export default {
-    components: { SectionMission, SectionShop, SectionQuote },
+    components: { SectionMission, SectionShop, SectionQuote, SectionOffers },
     props: { sectionProducts: Array, shopProducts: Array }
 }
 </script>
@@ -17,5 +18,7 @@ export default {
         <SectionShop :foods="shopProducts" />
         <!-- quote -->
         <SectionQuote />
+        <!-- offers -->
+        <SectionOffers />
     </main>
 </template>
