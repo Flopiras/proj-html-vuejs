972deb8b979b0e1b03dac0e90316e4c5557b3738
Flopiras <Flopiras@yahoo.it>
2023-07-11 12:30:22 +0200

    add SectionShop

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,18 @@
 <script>
 import SectionMission from './main/SectionMission.vue';
+import SectionShop from './main/SectionShop.vue';
 
 export default {
-    components: { SectionMission },
+    components: { SectionMission, SectionShop },
     props: { sectionProducts: Array }
 }
 </script>
 
 <template>
     <main>
+        <!-- mission -->
         <SectionMission :products="sectionProducts" />
+        <!-- shop -->
+        <SectionShop />
     </main>
 </template>
