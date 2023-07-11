d29eba0bf4cd2a19f3a0df52badf92325e56e2c1
Flopiras <Flopiras@yahoo.it>
2023-07-11 17:24:42 +0200

    add OffersCards

--- src/components/main/SectionOffers.vue
+++ src/components/main/SectionOffers.vue
@@ -7,15 +7,41 @@ export default {
 </script>
 
 <template>
-    <div class="section">
+    <section>
         <div class="container">
             <div class="row">
-                <div class="col-2"></div>
-                <div class="col-4"></div>
-                <div class="col-4"></div>
-                <div class="col-2"></div>
+                <div class="col-1"></div>
+                <div class="left-offer col-5">
+                    <OfferCard :text="'Fresh gurden tomato combo offer... $37'" />
+                </div>
+                <div class="right-offer col-5">
+                    <OfferCard :text="'Some organic healty fruits combo offer... $49'" />
+                </div>
+                <div class="col-1"></div>
             </div>
         </div>
-        <OfferCard />
-    </div>
+    </section>
 </template>
+
+<style lang="scss" scoped>
+section {
+    margin-bottom: 5rem;
+}
+
+.left-offer {
+    margin-right: 1rem;
+    background-image: url(../../assets/img/offer-img01.jpg);
+}
+
+.right-offer {
+    margin-left: 1rem;
+    background-image: url(../../assets/img/offer-img02.jpg);
+}
+
+.left-offer,
+.right-offer {
+    height: 250px;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
