5ac01328d75dfa3c7359904e4544e85d68b37231
Flopiras <Flopiras@yahoo.it>
2023-07-11 16:31:02 +0200

    SectionQuote

---
+++ src/components/main/SectionQuote.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="text-center">
+        <div class="container">
+            <div class="marks">
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/sass/vars' as *;
+
+section {
+    margin-top: 8rem;
+}
+</style>
