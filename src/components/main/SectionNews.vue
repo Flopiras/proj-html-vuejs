8ef126e124ac564f2729a277b3030d6cdc692046
Flopiras <Flopiras@yahoo.it>
2023-07-12 11:58:12 +0200

    add section-title

--- src/components/main/SectionNews.vue
+++ src/components/main/SectionNews.vue
@@ -1,1 +1,28 @@
-<template></template>
+<template>
+    <section>
+        <div class="section-title">
+            <h5 class="second-color">The news what we have</h5>
+            <h3 class="main-color text-uppercase">gogrin latest <span class="second-color">news</span> feed</h3>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.section-title {
+    text-align: center;
+    padding-top: 3rem;
+    background-image: url(../../assets/img/title-shap.png);
+    background-repeat: no-repeat;
+    background-position: 50%;
+
+    h5 {
+        font-weight: 600;
+    }
+
+    h3 {
+        font-size: 2.3rem;
+        font-weight: 600;
+    }
+
+}
+</style>
