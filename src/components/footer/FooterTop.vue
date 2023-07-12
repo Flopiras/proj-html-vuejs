3410fdf49411de8019734303a2cffcd9f3eda7c6
Flopiras <Flopiras@yahoo.it>
2023-07-12 14:56:05 +0200

    add logo and description

--- src/components/footer/FooterTop.vue
+++ src/components/footer/FooterTop.vue
@@ -1,21 +1,30 @@
 <script>
 import BaseFormSubscribe from '../BaseFormSubscribe.vue';
+import BaseCard from '../BaseCard.vue';
 export default {
-    components: { BaseFormSubscribe }
+    components: { BaseFormSubscribe, BaseCard }
 }
 </script>
 
 <template>
     <section>
-        <div class="container">
+        <div class="container text-white">
             <!-- form -->
             <BaseFormSubscribe />
             <!-- links -->
-            <div class="row">
+            <div class="row links">
                 <div class="col-2"></div>
+                <!-- logo and socials -->
+                <div class="col">
+                    <BaseCard :pic="'footer-logo.png'" :name="'logo'" />
+
+                    <p>War should never be entered upon until every agency of piece has failed not be good</p>
+                </div>
+                <!-- emergency links -->
                 <div class="col"></div>
+                <!-- blog -->
                 <div class="col"></div>
-                <div class="col"></div>
+                <!-- gallery -->
                 <div class="col"></div>
                 <div class="col-2"></div>
             </div>
@@ -33,8 +42,12 @@ section {
     #base-form {
         width: calc(100% / 12 * 8);
         position: absolute;
-        top: -45%;
+        top: -20%;
         right: 17%;
     }
+}
+
+.links {
+    font-size: 0.8rem;
 }
 </style>
