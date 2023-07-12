86293a3ed441b1d288da8984483defda2948bcd6
Flopiras <Flopiras@yahoo.it>
2023-07-12 13:47:34 +0200

    boilerplate sectionPartners

--- src/components/main/SectionPartners.vue
+++ src/components/main/SectionPartners.vue
@@ -1,1 +1,24 @@
-<template></template>
+<script>
+import BaseCard from '../BaseCard.vue';
+export default {
+    components: { BaseCard }
+}
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+                    <div class="grid container-fluid">
+                        <div class="row row-cols-5">
+                            <div class="col"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-2"></div>
+            </div>
+        </div>
+    </section>
+</template>
