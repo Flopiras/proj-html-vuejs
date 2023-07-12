af3bc3540492b533d00924ec43634576f04fb407
Flopiras <Flopiras@yahoo.it>
2023-07-12 14:05:23 +0200

    add background

--- src/components/footer/FooterTop.vue
+++ src/components/footer/FooterTop.vue
@@ -1,1 +1,18 @@
-<template></template>
+<script>
+export default {
+    components: {}
+}
+</script>
+
+<template>
+    <section>
+
+    </section>
+</template>
+
+<style scoped>
+section {
+    background-image: url(../../assets/img/footer-bg.jpg);
+    padding-bottom: 6rem;
+}
+</style>
