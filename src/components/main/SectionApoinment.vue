47878d185db554420f4c2ba14cf0b52077e8a39f
Flopiras <Flopiras@yahoo.it>
2023-07-12 11:13:25 +0200

    add background

--- src/components/main/SectionApoinment.vue
+++ src/components/main/SectionApoinment.vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+    <section>
+        <div class="container">
+
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    background-image: url(../../assets/img/call-to-bg.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    padding: 3rem 0;
+}
+</style>
